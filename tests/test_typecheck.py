d64ae4de863e2a21bd41836f2222cbb8a5888bfa
Mark <mverleg.noreply@gmail.com>
2024-09-06 22:43:55 +0200

    Support sub, mul, div the same way as add

--- tests/test_typecheck.py
+++ tests/test_typecheck.py
@@ -1,4 +1,4 @@
-from typecheck.syntree import Add, IntLiteral, TextLiteral
+from typecheck.syntree import Add, Sub, Mul, Div, IntLiteral, TextLiteral
 from typecheck.typ import Int
 from typecheck.typecheck import check
 
@@ -11,7 +11,10 @@ def test_number_addition():
     assert check(Add(IntLiteral(1), IntLiteral(2))) == Int
 
 
-def test_string_addition():
+def test_string_addition_etc():
     assert check(Add(IntLiteral(1), TextLiteral('hello'))) == 'no variant of Add for arguments (Int, Text)'
+    assert check(Sub(TextLiteral('hello'), IntLiteral(1))) == 'no variant of Sub for arguments (Text, Int)'
+    assert check(Mul(IntLiteral(1), TextLiteral('hello'))) == 'no variant of Mul for arguments (Int, Text)'
+    assert check(Div(TextLiteral('hello'), IntLiteral(1))) == 'no variant of Div for arguments (Text, Int)'
 
 
--- typecheck/typecheck.py
+++ typecheck/typecheck.py
@@ -1,6 +1,6 @@
 from typing_extensions import assert_never
 
-from typecheck.syntree import Expression, IntLiteral, Add, RealLiteral, TextLiteral
+from typecheck.syntree import Expression, IntLiteral, Add, Sub, Mul, Div, RealLiteral, TextLiteral
 from typecheck.typ import Type, Int, Real, Text
 
 
@@ -11,16 +11,20 @@ def check(expr: Expression) -> Type | str:
         return Real
     elif isinstance(expr, TextLiteral):
         return Text
-    elif isinstance(expr, Add):
-        left_type = check(expr.left)
-        right_type = check(expr.right)
-        if left_type == right_type == Int:
-            return Int
-        elif left_type == right_type == Real:
-            return Real
-        else:
-            return f'no variant of Add for arguments ({left_type}, {right_type})'
+    elif isinstance(expr, (Add, Sub, Mul, Div,)):
+        return binary_nr_func(expr)
     #TODO @mark: more
     assert_never(expr)
+
+
+def binary_nr_func(expr):
+    left_type = check(expr.left)
+    right_type = check(expr.right)
+    if left_type == right_type == Int:
+        return Int
+    elif left_type == right_type == Real:
+        return Real
+    else:
+        return f'no variant of {type(expr).__name__} for arguments ({left_type}, {right_type})'
 
 
